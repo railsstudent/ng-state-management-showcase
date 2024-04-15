5cc5cd870202b6d990e397ce945bad081fc63fff
Connie Leung <cleung334@gmail.com>
2024-04-15 22:56:25 +0800

    chore: delete boilerplate codes

--- projects/ng-signal-demo/src/app/app.component.ts
+++ projects/ng-signal-demo/src/app/app.component.ts
@@ -1,13 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [],
+  template: '<div>testing</div>',
+  styles: '',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'ng-signal-demo';
-}
+export class AppComponent {}
